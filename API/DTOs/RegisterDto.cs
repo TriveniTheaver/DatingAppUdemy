6a398df632cdead96e045b10251319492ff48de0
Triveni Tanvi Theaver <your-github-trivtheaver@gmail.com>
2026-04-15 10:42:57 +0530

    End of section 7

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -7,10 +7,12 @@ public class RegisterDto
 {
     [Required]
     public  string  DisplayName { get; set; } = "";
+
+
     [Required]
     [EmailAddress]
-
     public  string Email { get; set; } = "";
+    
 
     [Required]
     [MinLength(4)]
--- API/Program.cs
+++ API/Program.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using System.Text;
 using Microsoft.IdentityModel.Tokens;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -39,6 +40,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod()
     .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
